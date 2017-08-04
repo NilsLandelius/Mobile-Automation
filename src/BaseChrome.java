3bd6542088ce57fd06002ab9e50cb01d71703c9c
Landelius <nils.landelius@sogeti.com>
2017-08-04 13:15:18 +0200

    Physical device testing and automating browsers on mobile platforms.

---
+++ src/BaseChrome.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Base {
+    public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException
+    {
+
+
+        AndroidDriver<AndroidElement>  driver;
+
+        // TODO Auto-generated method stub
+        File appDir = new File("src");
+        File app = new File(appDir, "ApiDemos-debug.apk");
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Demo");
+            //cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android device");
+
+        //cap.setCapability("--session-override",true);
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+        return driver;
+    }
+
+
+
+}
---
+++ src/HybridBase.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by nlandeli on 2017-08-04.
+ */
+public class HybridBase {
+}
